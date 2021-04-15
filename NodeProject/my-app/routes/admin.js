4e9f8268b793fb1c9042cac916f8da10c48dacc7
YoungJaeLee <dudwo8075@gmail.com>
2021-04-15 13:52:23 +0900

    Crawling Test.

--- NodeProject/my-app/routes/admin.js
+++ NodeProject/my-app/routes/admin.js
@@ -250,24 +250,24 @@ async function contentsCrawling(page) {
 //     console.log("list Crawling End.")
 // })
 
-cron.schedule("*/10 * * * * *", async function () {
-    console.log("list Crawling Start.")
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-    const page = await browser.newPage()
-    await page.setViewport({
-        width: 1920,
-        height: 1080
-    })
-
-    await listCrawling(page)
-
-    await page.waitFor(5000)
-    await browser.close()
-    console.log("list Crawling End.")
-})
+// cron.schedule("*/10 * * * * *", async function () {
+//     console.log("list Crawling Start.")
+//     const browser = await puppeteer.launch({
+//         headless: false,
+//         args: ['--no-sandbox', '--disable-setuid-sandbox']
+//     })
+//     const page = await browser.newPage()
+//     await page.setViewport({
+//         width: 1920,
+//         height: 1080
+//     })
+//
+//     await listCrawling(page)
+//
+//     await page.waitFor(5000)
+//     await browser.close()
+//     console.log("list Crawling End.")
+// })
 
 // cron.schedule("43 13 * * *", async function () {
 //     console.log("Contents Crawling Start.")
@@ -289,25 +289,25 @@ cron.schedule("*/10 * * * * *", async function () {
 //     console.log("Contents Crawling End.")
 // })
 
-// cron.schedule("*/30 * * * * *", async function () {
-//     console.log("Contents Crawling Start.")
-//     const browser = await puppeteer.launch({
-//         headless: false,
-//         args: ['--no-sandbox', '--disable-setuid-sandbox']
-//     })
-//     const page = await browser.newPage()
-//     await page.setViewport({
-//         width: 1920,
-//         height: 1080
-//     })
-//
-//     await contentsCrawling(page)
-//
-//     await page.waitFor(10000)
-//     await browser.close()
-//
-//     console.log("Contents Crawling End.")
-// })
+cron.schedule("*/10 * * * * *", async function () {
+    console.log("Contents Crawling Start.")
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    const page = await browser.newPage()
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    })
+
+    await contentsCrawling(page)
+
+    await page.waitFor(10000)
+    await browser.close()
+
+    console.log("Contents Crawling End.")
+})
 
 /**
  * --------------------------------------------------------------------------------------------------------
