0c5072fe4aff51058a4503374daa9eda97ef7681
YoungJaeLee <dudwo8075@gmail.com>
2021-04-15 11:15:39 +0900

    Crawling Test.

--- NodeProject/my-app/routes/admin.js
+++ NodeProject/my-app/routes/admin.js
@@ -231,7 +231,23 @@ async function contentsCrawling(page) {
     })
 }
 
-cron.schedule("11 11 1-31 * *", async function () {
+// cron.schedule("12 11 1-31 * *", async function () {
+//     console.log("list Crawling Start.")
+//     const browser = await puppeteer.launch({headless: false})
+//     const page = await browser.newPage()
+//     await page.setViewport({
+//         width: 1920,
+//         height: 1080
+//     })
+//
+//     await listCrawling(page)
+//
+//     await page.waitFor(5000)
+//     await browser.close()
+//     console.log("list Crawling End.")
+// })
+
+cron.schedule("*/10 * * * * *", async function () {
     console.log("list Crawling Start.")
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
@@ -247,7 +263,7 @@ cron.schedule("11 11 1-31 * *", async function () {
     console.log("list Crawling End.")
 })
 
-cron.schedule("12 11 1-31 * *", async function () {
+cron.schedule("13 11 1-31 * *", async function () {
     console.log("Contents Crawling Start.")
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
