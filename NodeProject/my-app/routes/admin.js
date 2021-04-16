b0c93f76b3d33a0260a84e05eee264f1cb9c4384
YoungJaeLee <dudwo8075@gmail.com>
2021-04-16 11:25:36 +0900

    API Test.

--- NodeProject/my-app/routes/admin.js
+++ NodeProject/my-app/routes/admin.js
@@ -27,7 +27,7 @@ app.use(sessionConfig.init())
  * Rank Scheduler 00 00 * * 1 매주 월요일 00 시 00분.
  * --------------------------------------------------------------------------------------------------------
  */
-cron.schedule("22 11 * * 1", function () {
+cron.schedule("26 11 * * 1", function () {
     // 정지 여부, 탈퇴 여부, 포인트 조회 후 정렬
     let searchPointSql = "select member_email, save_point, member_log_join\n" +
         "from (select m.member_email,\n" +
@@ -249,7 +249,7 @@ async function contentsCrawling(page) {
     })
 }
 
-cron.schedule("23 11 * * *", async function () {
+cron.schedule("27 11 * * *", async function () {
     console.log("list Crawling Start.")
     const browser = await puppeteer.launch({
         headless: false,
@@ -271,7 +271,7 @@ cron.schedule("23 11 * * *", async function () {
     timezone: "Asia/Seoul"
 })
 
-cron.schedule("24 11 * * *", async function () {
+cron.schedule("28 11 * * *", async function () {
     console.log("Contents Crawling Start.")
     const browser = await puppeteer.launch({
         headless: false,
